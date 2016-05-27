c47c0c04844693e69484e44876c4dc04d049f54e
Mark Jordan <mjordan@sfu.ca>
2016-05-27 00:54:25 +0000

    Address #3.

--- theme/islandora_simple_map.tpl.php
+++ theme/islandora_simple_map.tpl.php
@@ -12,9 +12,11 @@
  *     Width of the map iframe.
  *   $zoom string
  *     The default zoom level.
+ *   $collapsed string
+ *     The string 'collapsed' or ''.
  */
 ?>
-<fieldset class="islandora collapsible collapsed">
+<fieldset class="islandora collapsible <?php print $collapsed; ?>">
   <legend><span class="fieldset-legend"><?php print t('Map'); ?></span></legend>
   <div class="fieldset-wrapper">
     <iframe width="<?php print $iframe_width; ?>" height="<?php print $iframe_height; ?>" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?hl=en&amp;q=<?php print $coords; ?>&amp;iwloc=A&amp;z=<?php print $zoom; ?>&amp;t=m&amp;output=embed"></iframe>
