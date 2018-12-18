1615284fab9dbf6e7cff78edf33e90b89e55f74a
Bryce Gilhome <bryce@monkii.com>
2018-12-18 07:22:22 -0800

    Add the new hook_islandora_simple_map_get_kml_alter() to the api file.

--- islandora_simple_map.api.php
+++ islandora_simple_map.api.php
@@ -107,3 +107,16 @@ function hook_islandora_simple_map_get_kml(AbstractObject $object) {
     'http://googlemaps.github.io/kml-samples/kml/Placemark/placemark.kml',
   );
 }
+
+/**
+ * Permit altering of KML hook values.
+ *
+ * @param array $kml
+ *   A reference to the array of KML documents gathered.
+ * @param AbstractObject $object
+ *   The object for which KML is being gathered.
+ *
+ * @see hook_islandora_gmap_get_kml()
+ */
+function hook_islandora_simple_map_get_kml_alter(array &$kml, AbstractObject $object) {
+}
