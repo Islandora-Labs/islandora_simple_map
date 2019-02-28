23bb35d2b4828a22f7082423ea0bc8e768bd6d29
Bryce Gilhome <me@bryce.com.au>
2019-02-28 08:48:53 -0800

    Add GeoJSON handling to openstreet_maps.js.

--- js/openstreet_maps.js
+++ js/openstreet_maps.js
@@ -17,17 +17,35 @@
           var multiple_objects = (Object.prototype.toString.call(config.map_markers) === '[object Array]' ? true : false);
           var bounds = new L.LatLngBounds();
           var coords = [];
-          if (multiple_objects) {
-            var firstCenter = Drupal.islandora_simple_map.coordToPoint(config.map_markers[0].coordinates[0]);
-          } else {
-            var firstCenter = Drupal.islandora_simple_map.coordToPoint(config.map_markers.coordinates[0]);
+          var firstCenter = new L.LatLng(0, 0);
+          if (multiple_objects && config.map_markers[0].coordinates.length) {
+            firstCenter = Drupal.islandora_simple_map.coordToPoint(config.map_markers[0].coordinates[0]);
+          } else if (config.map_markers.coordinates.length) {
+            firstCenter = Drupal.islandora_simple_map.coordToPoint(config.map_markers.coordinates[0]);
           }
+          else if (config.map_geojson && config.map_geojson.features.length) {
+            var coordArray = config.map_geojson.features[0].geometry.coordinates;
+            // NB GeoJSON coordinates are [ lng, lat ]. Array.reverse() alters the object so don't use it yet.
+            firstCenter = Drupal.islandora_simple_map.coordToPoint(`${coordArray[1]},${coordArray[0]}`);
+          }
+
           var defaultZoom = parseInt(config.map_zoom_level);
           var map = L.map(mapId).setView(firstCenter, defaultZoom);
           var default_config = config.provider_settings.tile_config;
           L.tileLayer(config.provider_settings.template, default_config).addTo(map);
           map.defaultZoom = defaultZoom;
           map.initialZoom = true;
+
+          if (config.map_geojson && config.map_geojson.features.length) {
+            config.map_geojson.features.forEach(function (feature) {
+              L.geoJSON(feature, {
+                onEachFeature: function (feature) {
+                    Drupal.islandora_simple_map.makePoint(map, bounds, pid, feature.geometry.coordinates.reverse().join(','));
+                }
+              }).addTo(map);
+            });
+          }
+
           if (multiple_objects) {
             config.map_markers.forEach(function (objVal) {
               var pid = objVal.pid;
@@ -79,8 +97,10 @@
       }
     },
     coordToPoint: function (coordinate) {
-      var c = coordinate.split(',');
-      return new L.LatLng(parseFloat(c[0].trim()), parseFloat(c[1].trim()));
+      if (coordinate) {
+        var c = coordinate.split(',');
+        return new L.LatLng(parseFloat(c[0].trim()), parseFloat(c[1].trim()));
+      }
     },
     makePoint: function(map, bounds, pid, coordinate) {
       lPoint = Drupal.islandora_simple_map.coordToPoint(coordinate);
@@ -94,10 +114,10 @@
       Drupal.islandora_simple_map.markers.push(marker);
     },
     redraw: function(map, bounds) {
-      map.fitBounds(bounds);
-      map.invalidateSize();
-      map.panTo(bounds.getCenter());
-      map.flyToBounds(bounds);
+      //map.fitBounds(bounds);
+      //map.invalidateSize();
+      //map.panTo(bounds.getCenter());
+      //map.flyToBounds(bounds);
     },
     markerClick: function(map, marker) {
       var pid = marker.pid;
