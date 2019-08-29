d10e628bae3dcf9463099d4f9ae51274bb58abf0
Jared Whiklo <jwhiklo@gmail.com>
2019-08-29 09:52:32 -0500

    Refactor javascript, make geoJson include a pid

--- js/google_maps.js
+++ js/google_maps.js
@@ -1,155 +1,151 @@
 (function (jQuery) {
-  Drupal.islandora_simple_map = {
-    maps: {},
-    markers: [],
-    initialize: function() {
-      jQuery('.islandora-simple-map-holder').once('islandora-simple-map', function(){
-        Drupal.islandora_simple_map.initMap(this.id);
+
+  IslandoraGoogleSimpleMap = function(mapId, settings) {
+    IslandoraSimpleMap.call(this, mapId, settings);
+    this.bounds = new google.maps.LatLngBounds();
+  };
+
+  jQuery.extend(IslandoraGoogleSimpleMap.prototype, IslandoraSimpleMap.prototype);
+
+  IslandoraGoogleSimpleMap.prototype.initMap = function() {
+    return new google.maps.Map(this.map_div, {
+      zoom: parseInt(this.config.map_zoom_level),
+      center: this.coords[0],
+      scrollwheel: (!this.config.disable_scroll_zoom)
+    });
+  };
+
+  IslandoraGoogleSimpleMap.prototype.doKmlLayers = function(kml_layers) {
+    this.config.map_kml.forEach(function (url) {
+      var layer = new google.maps.KmlLayer(url, {
+        preserveViewport: true,
+        map: this.map
       });
-    },
-    initMap: function(mapId) {
-      if (typeof(Drupal.settings.islandora_simple_map) != 'undefined') {
-        if (typeof(Drupal.settings.islandora_simple_map[mapId]) != 'undefined') {
-          var config = Drupal.settings.islandora_simple_map[mapId];
-          var map_div = jQuery('#' + mapId).get(0);
-          var multiple_objects = (Object.prototype.toString.call(config.map_markers) === '[object Array]' ? true : false);
-          var bounds = new google.maps.LatLngBounds();
-          var coords = [];
-          var map = new google.maps.Map(map_div, {
-            zoom: parseInt(config.map_zoom_level),
-            center: coords[0],
-            scrollwheel: (!config.disable_scroll_zoom)
-          });
-          map.defaultZoom = map.getZoom();
-          map.initialZoom = true;
+    });
+  };
 
-          if (config.map_geojson && config.map_geojson.features.length) {
-            // Add a listener to extend bounds whenever a geojson feature is added.
-            map.data.addListener('addfeature', function(e) {
-              Drupal.islandora_simple_map.processPoint(e.feature.getGeometry(), bounds);
-            });
+  IslandoraGoogleSimpleMap.prototype.markerEventListener = function(marker) {
+    var outerObj = this;
+    marker.addListener('click', function () {
+      outerObj.markerClick(this);
+    });
+  };
 
-            // Add the geojson.
-            map.data.addGeoJson(config.map_geojson);
-          }
-
-          if (config.map_kml.length) {
-            config.map_kml.forEach(function (url) {
-              var layer = new google.maps.KmlLayer(url, {
-                preserveViewport: true,
-                map: map
-              });
-            });
-          }
-
-          if (multiple_objects) {
-            config.map_markers.forEach(function (objVal) {
-              var pid = objVal.pid;
-              objVal.coordinates.forEach(function (coordVal) {
-                Drupal.islandora_simple_map.makePoint(map, bounds, pid, coordVal);
-              });
-            });
-            // Only do infoWindows when there are multiple objects.
-            Drupal.islandora_simple_map.markers.forEach(function(marker) {
-              google.maps.event.addListener(marker, 'click', function() {
-                Drupal.islandora_simple_map.markerClick(map, this);
-              });
-            });
-            google.maps.event.addListener(map, 'click', function() {
-              if (typeof(Drupal.islandora_simple_map.mapInfoWindow) != 'undefined'){
-                Drupal.islandora_simple_map.mapInfoWindow.close();
-              }
-            });
-          } else {
-            var pid = config.map_markers.pid;
-            config.map_markers.coordinates.forEach(function(coordVal) {
-              Drupal.islandora_simple_map.makePoint(map, bounds, pid, coordVal);
-            });
-          }
-          fieldsets = jQuery(map_div).closest('.collapsible.collapsed')
-          if (fieldsets.length > 0) {
-            for (var f = 0; f < fieldsets.length; f += 1) {
-              jQuery(fieldsets[f]).one('collapsed', function() {
-                window.setTimeout(function () {
-                  Drupal.islandora_simple_map.redraw(map, bounds);
-                }, 200);
-              });
-            }
-          }
-          else {
-            Drupal.islandora_simple_map.redraw(map, bounds);
-          }
-          // On first zoom called from map.fitBounds ensure we don't zoom closer.
-          google.maps.event.addListener(map, 'zoom_changed', function() {
-            if (map.initialZoom) {
-              if (map.getZoom() > map.defaultZoom) {
-                map.setZoom(map.defaultZoom);
-              }
-              map.initialZoom = false;
-            }
-          });
-          Drupal.islandora_simple_map.maps[mapId] = map;
-        }
+  IslandoraGoogleSimpleMap.prototype.mapEventListener = function(map) {
+    var outerObj = this;
+    google.maps.event.addDomListener(map, 'click', function() {
+      if (typeof(outerObj.mapInfoWindow) != 'undefined'){
+        outerObj.mapInfoWindow.close();
       }
-    },
-    makePoint: function(map, bounds, pid, coordinate) {
-      var c = coordinate.split(',');
-      var lPoint = new google.maps.LatLng(parseFloat(c[0].trim()), parseFloat(c[1].trim()));
-      bounds.extend(lPoint);
-      var marker = new google.maps.Marker({
-        position: lPoint,
-        map: map
-      });
-      marker.pid = pid;
-      Drupal.islandora_simple_map.markers.push(marker);
-    },
-    processPoint: function(geometry, bounds) {
-      if (geometry instanceof google.maps.LatLng) {
-        bounds.extend(geometry);
-      } else if (geometry instanceof google.maps.Data.Point) {
-        bounds.extend(geometry.get());
-      } else {
-        geometry.getArray().forEach(function(g) {
-          Drupal.islandora_simple_map.processPoint(g, bounds);
-        });
-      }
-    },
-    redraw: function(map, bounds) {
-      google.maps.event.trigger(map, 'resize');
-      map.fitBounds(bounds);
-      map.panTo(bounds.getCenter());
-    },
-    markerClick: function(map, marker) {
-      var pid = marker.pid;
-      jQuery.ajax('/islandora_simple_map/marker/' + encodeURI(pid),{
-        "dataType" : "json",
-        "success" : function(data) {
-          Drupal.islandora_simple_map.openInfoWindow(map, marker, data);
+    });
+  };
+
+  IslandoraGoogleSimpleMap.prototype.zoomChangedEventListener = function(map) {
+    // On first zoom called from map.fitBounds ensure we don't zoom closer.
+    google.maps.event.addDomListener(map, 'zoom_changed', function() {
+      if (map.initialZoom) {
+        if (map.getZoom() > map.defaultZoom) {
+          map.setZoom(map.defaultZoom);
         }
-      })
-    },
-    openInfoWindow: function(map, marker, objectData) {
-      var content = '<div id="islandora-simple-map-infowindow" class="islandora-simple-map-infowindow-content">' +
-          '<div class="islandora-simple-map-infowindow-title">' +
+        map.initialZoom = false;
+      }
+    });
+  };
+
+  IslandoraGoogleSimpleMap.prototype.openInfoWindow = function(map, marker, objectData) {
+    var content = '<div id="islandora-simple-map-infowindow" class="islandora-simple-map-infowindow-content">' +
+        '<div class="islandora-simple-map-infowindow-title">' +
+        '<a href="' + objectData.uri +'">' +
+        objectData.label +
+        '</a>' +
+        '</div>';
+    if (typeof(objectData.thumbnail_uri) != 'none') {
+      content += '<div class="islandora-simple-map-infowindow-thumbnail">' +
           '<a href="' + objectData.uri +'">' +
-          objectData.label +
+          '<img src="' + objectData.thumbnail_uri + '"' +
+          'class="islandora-simple-map-infowindow-thumbnail-img"/>' +
           '</a>' +
           '</div>';
-      if (typeof(objectData.thumbnail_uri) != 'none') {
-        content += '<div class="islandora-simple-map-infowindow-thumbnail">' +
-            '<a href="' + objectData.uri +'">' +
-            '<img src="' + objectData.thumbnail_uri + '"' +
-            'class="islandora-simple-map-infowindow-thumbnail-img"/>' +
-            '</a>' +
-            '</div>';
-      }
-      content += '</div>';
-      if (typeof(Drupal.islandora_simple_map.mapInfoWindow) == 'undefined') {
-        Drupal.islandora_simple_map.mapInfoWindow = new google.maps.InfoWindow();
-      }
-      Drupal.islandora_simple_map.mapInfoWindow.close();
-      Drupal.islandora_simple_map.mapInfoWindow.setContent(content);
-      Drupal.islandora_simple_map.mapInfoWindow.open(map, marker);
+    }
+    content += '</div>';
+    if (typeof(this.mapInfoWindow) == 'undefined') {
+      this.mapInfoWindow = new google.maps.InfoWindow();
+    }
+    this.mapInfoWindow.close();
+    this.mapInfoWindow.setContent(content);
+    if (marker instanceof google.maps.Data.Feature) {
+      // anchor the infowindow on the marker
+      this.mapInfoWindow.setPosition(marker.getGeometry().get());
+      this.mapInfoWindow.open(map);
+    } else {
+      this.mapInfoWindow.open(map, marker);
+    }
+  };
+
+  IslandoraGoogleSimpleMap.prototype.doGeoJson = function(geojsons) {
+    var outerObj = this;
+    // Add a listener to extend bounds whenever a geojson feature is added.
+    this.map.data.addListener('addfeature', function(f) {
+      outerObj.processPoint(f.feature);
+    });
+    if (this.multiple_objects) {
+      this.map.data.addListener('click', function(event) {
+        outerObj.featureClick(event.feature);
+      });
+    }
+
+    // Add the geojson.
+    this.map.data.addGeoJson(this.config.map_geojson);
+  }
+
+  IslandoraGoogleSimpleMap.prototype.processPoint = function(feature) {
+    var outerObj = this;
+    var geometry = feature.getGeometry();
+    var pid = feature.getProperty('pid');
+
+    if (geometry instanceof google.maps.LatLng) {
+      this.makeMarker(geometry, pid);
+    } else if (geometry instanceof google.maps.Data.Point) {
+      this.makeMarker(geometry.get(), pid);
+    } else {
+      geometry.getArray().forEach(function(g) {
+        outerObj.processPoint(g);
+      });
+    }
+  };
+
+  IslandoraGoogleSimpleMap.prototype.makePoint = function(bounds, pid, coordinate) {
+    var c = coordinate.split(',');
+    var lPoint = new google.maps.LatLng(parseFloat(c[0].trim()), parseFloat(c[1].trim()));
+    this.makeMarker(lPoint, pid);
+  };
+
+  IslandoraGoogleSimpleMap.prototype.makeMarker = function(lPoint, pid) {
+    this.bounds.extend(lPoint);
+    var marker = new google.maps.Marker({
+      position: lPoint,
+      map: this.map
+    });
+    marker.pid = pid;
+    this.markers.push(marker);
+  };
+
+  IslandoraGoogleSimpleMap.prototype.redraw = function() {
+    google.maps.event.trigger(this.map, 'resize');
+    this.map.fitBounds(this.bounds);
+    this.map.panTo(this.bounds.getCenter());
+  };
+
+  Drupal.islandora_simple_map = {
+    maps: {},
+    initialize: function() {
+      jQuery('.islandora-simple-map-holder').once('islandora-simple-map', function(){
+        var theMap = new IslandoraGoogleSimpleMap(this.id, Drupal.settings.islandora_simple_map);
+        if (theMap.loaded) {
+          theMap.initialize();
+          Drupal.islandora_simple_map.maps[this.id] = theMap;
+        }
+      });
     }
   }
 })(jQuery);
